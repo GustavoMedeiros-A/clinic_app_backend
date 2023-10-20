f6024d0e416a8fbad896c265d3d5c4478892fa14
Gustavo Medeiros <gustavomedeiros15@outlook.com.br>
2023-10-20 17:40:00 -0300

    feat: fix port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 
+const port = 3000;
+
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
