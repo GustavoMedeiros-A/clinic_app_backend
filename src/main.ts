9d473fa4c63ab33cd601584efbd35b46b53be7a5
Gustavo Medeiros <gustavomedeiros15@outlook.com.br>
2023-10-20 17:43:20 -0300

    feat: fix port again

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 
-const port = 3000;
-
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(port, '0.0.0.0');
+  await app.listen(process.env.RAILWAY_MYSQL_PORT, '0.0.0.0');
 }
 bootstrap();
